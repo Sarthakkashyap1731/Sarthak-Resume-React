5bef833508676014777f01ae3ffa72840516b06b
Sarthakkashyap1731 <kashyapsarthak177@gmail.com>
2021-10-22 11:48:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './Header/Header'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ src/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div class="jumbotron bg-light">
+      <div class="container">
+        <div class="row">
+        <div class="col-lg-4">
+            <img
+              src="https://www.static-contents.youth4work.com/y4w/Images/UserThumbImage/160_160/20200522094033.png?v=20200522094033"
+              alt="abc"
+            />
+          </div>
+          <div class="col-lg-4">
+            <h1 class="display-4 ">Sarthak Kashyap</h1>
+            <p>
+              Interested in the role of a Software Engineer where I can
+              contribute my analytical skills and knowledge completed my degree
+              in Software Engineering. Looking for a dynamic and progressive
+              company where my skills are utilized in the maximum way possible.
+            </p>
+          </div>
+          
+        </div>
+
+        <hr class="my-4"></hr>
+        <p>Contact Details: </p>
+        <p>Number : 9826992097</p>
+        <p>Email : kashyapsarthak177@gmail.com</p>
+        <p>Location : Indore,India</p>
+        <p>LinkdeIn : https://www.linkedin.com/in/sarthak-kashyap-4b536a193</p>
+        <hr class="my-4"></hr>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
